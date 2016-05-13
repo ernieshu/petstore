55ea0263840cc9445725bcc31aae1ee5986e3f93
Ernie Shu <ernie.shu@intelliware.com>
2016-05-13 15:50:18 -0400

    Adding access to DB via console.

--- petstore-services/src/main/java/com/iwd/petstore_services/AppController.java
+++ petstore-services/src/main/java/com/iwd/petstore_services/AppController.java
@@ -1,8 +1,11 @@
 package com.iwd.petstore_services;
 
+import org.h2.server.web.WebServlet;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -35,6 +38,13 @@ public class AppController {
 	@ResponseBody
 	void delete(@PathVariable Long petId) {
 		petStoreService.delete(petId);
+	}
+
+	@Bean
+	public ServletRegistrationBean h2servletRegistration() {
+		ServletRegistrationBean registration = new ServletRegistrationBean(new WebServlet());
+		registration.addUrlMappings("/console/*");
+		return registration;
 	}
 
 	public static void main(String[] args) {
