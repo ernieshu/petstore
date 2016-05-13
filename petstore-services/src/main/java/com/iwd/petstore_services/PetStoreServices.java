714d46b820d6b09e85015d8cad800f402d31cd92
Ernie Shu <ernie.shu@intelliware.com>
2016-05-13 12:33:08 -0400

    Skeleton for services.

---
+++ petstore-services/src/main/java/com/iwd/petstore_services/PetStoreServices.java
@@ -0,0 +1,19 @@
+package com.iwd.petstore_services;
+
+import com.iwd.petstore_services.domain.Pet;
+
+public class PetStoreServices {
+
+	public void addPet() {
+
+	}
+
+	public Pet findPet(Long petId) {
+		// TODO
+		return null;
+	}
+
+	public void deletePet(Long petId) {
+
+	}
+}
---
+++ petstore-services/src/main/java/com/iwd/petstore_services/domain/PetStatus.java
@@ -0,0 +1,7 @@
+package com.iwd.petstore_services.domain;
+
+public enum PetStatus {
+
+	DEAD, ALIVE
+
+}
