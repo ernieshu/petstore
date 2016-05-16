7e0b9d536adb293a205094253fbdd1e38e86208f
Ernie Shu <ernie.shu@intelliware.com>
2016-05-16 15:24:04 -0400

    First cut of adding a pet.

--- petstore-ui/app/scripts/controllers/main.js
+++ petstore-ui/app/scripts/controllers/main.js
@@ -8,10 +8,26 @@
  * Controller of the petstoreUiApp
  */
 angular.module('petstoreUiApp')
-  .controller('MainCtrl', function ($scope) {
+  .controller('MainCtrl', function ($scope, $http) {
+
+  	console.log("Starting a console log of this working...!");
+
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
+
+    $scope.addAPet = function() {
+    	console.log("adding a pet with name:" + $scope.name);
+
+    	// $http.post('pet');
+    };
+
+    this.findAPet = function(data) {
+	  	$http.get('pet/' + data.petId).success(function(data) {
+	  		// if we've gotten the pet, map it back to the GUI
+	  		$scope.petName = data.name;
+	  	});
+    };
   });
