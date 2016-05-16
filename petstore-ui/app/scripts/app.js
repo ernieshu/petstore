24d96cc5f847b5ad2fd9d30141f5daf401c2d086
Ernie Shu <ernie.shu@intelliware.com>
2016-05-16 10:31:07 -0400

    First cut of UI.

---
+++ petstore-ui/app/scripts/app.js
@@ -0,0 +1,33 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name petstoreUiApp
+ * @description
+ * # petstoreUiApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('petstoreUiApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
