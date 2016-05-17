b56c6d39e158adfdeebdaf79b7e7f58a1b8e4b81
Ernie Shu <ernie.shu@intelliware.ca>
2016-05-17 16:22:35 -0400

    Adding extra test coverage.

---
+++ petstore-services/src/test/java/com/iwd/petstore_services/domain/PetStatusTest.java
@@ -0,0 +1,45 @@
+package com.iwd.petstore_services.domain;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
+public class PetStatusTest {
+
+	@Test
+	public void testGetValues_validCase() {
+
+		PetStatus returnPetStatus = PetStatus.getEnum(PetStatus.AVAILABLE.getStatusName());
+		assertEquals(PetStatus.AVAILABLE, returnPetStatus);
+
+		// test case sensitive case
+		returnPetStatus = PetStatus.getEnum(PetStatus.AVAILABLE.getStatusName().toUpperCase());
+		assertEquals(PetStatus.AVAILABLE, returnPetStatus);
+
+		returnPetStatus = PetStatus.getEnum(PetStatus.PENDING.getStatusName());
+		assertEquals(PetStatus.PENDING, returnPetStatus);
+
+		// test case sensitive case
+		returnPetStatus = PetStatus.getEnum(PetStatus.PENDING.getStatusName().toUpperCase());
+		assertEquals(PetStatus.PENDING, returnPetStatus);
+
+		returnPetStatus = PetStatus.getEnum(PetStatus.SOLD.getStatusName());
+		assertEquals(PetStatus.SOLD, returnPetStatus);
+
+		// test case sensitive case
+		returnPetStatus = PetStatus.getEnum(PetStatus.SOLD.getStatusName().toUpperCase());
+		assertEquals(PetStatus.SOLD, returnPetStatus);
+	}
+
+	@Test
+	public void testGetValues_invalidCase() {
+		try {
+			PetStatus returnPetStatus = PetStatus.getEnum("something invalid");
+			fail("Shouldn't get here - expect an exception!");
+		} catch (IllegalArgumentException iae) {
+			// this is the expected good condition ...
+		}
+	}
+
+}
