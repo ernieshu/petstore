1f309a87853e8f088ca64217063ad628e50a0f24
Ernie Shu <ernie.shu@intelliware.ca>
2016-05-16 22:13:17 -0400

    First cut of more complex data structure.

--- petstore-services/src/main/java/com/iwd/petstore_services/domain/Pet.java
+++ petstore-services/src/main/java/com/iwd/petstore_services/domain/Pet.java
@@ -1,11 +1,17 @@
 package com.iwd.petstore_services.domain;
 
 import java.io.Serializable;
+import java.util.Set;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.OneToOne;
 
 @Entity
 public class Pet implements Serializable {
@@ -16,7 +22,28 @@ public class Pet implements Serializable {
 
 	@Column
 	private String name;
+	
+	@Column
+	private PetStatus status;
 
+	@OneToOne(fetch=FetchType.EAGER)
+	@JoinColumn(name="CATEGORY_ID")
+	private Category category;
+	
+	// TODO phtotoURLS
+//	@OneToMany(orphanRemoval=true)
+//	@JoinColumn(name="PET_ID")
+//	public Set<String> getPhotoURLs {return photoURLs;}
+	
+    @ManyToMany
+    @JoinTable(name="PET_TAGS",
+        joinColumns=
+            @JoinColumn(name="PET_ID", referencedColumnName="ID"),
+        inverseJoinColumns=
+            @JoinColumn(name="TAG_ID", referencedColumnName="TAG_ID")
+        )
+	private Set<Tag> tags;
+	
 	public Long getId() {
 		return id;
 	}
