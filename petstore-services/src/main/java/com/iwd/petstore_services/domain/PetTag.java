359152d79eb96f2288fcaaa87277e19e39a9523b
Ernie Shu <ernie.shu@intelliware.com>
2016-05-17 17:38:49 -0400

    Fix typo.

--- petstore-services/src/main/java/com/iwd/petstore_services/domain/PetTag.java
+++ petstore-services/src/main/java/com/iwd/petstore_services/domain/PetTag.java
@@ -4,10 +4,8 @@ import java.io.Serializable;
 
 import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
-import javax.persistence.Table;
 
 @Entity
-@Table(name = "PET_TAGS")
 public class PetTag implements Serializable {
 
 	@EmbeddedId
