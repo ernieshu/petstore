4ccb0f200c2ea42b780687c4c5cd714a59089f85
Ernie Shu <ernie.shu@intelliware.com>
2016-05-18 12:42:02 -0400

    Improve package structure.

--- petstore-services/src/test/java/com/iwd/petstore/services/AppControllerTest.java
+++ petstore-services/src/test/java/com/iwd/petstore/services/AppControllerTest.java
@@ -1,4 +1,4 @@
-package com.iwd.petstore_services;
+package com.iwd.petstore.services;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
--- petstore-services/src/test/java/com/iwd/petstore/services/domain/PetStatusTest.java
+++ petstore-services/src/test/java/com/iwd/petstore/services/domain/PetStatusTest.java
@@ -1,4 +1,4 @@
-package com.iwd.petstore_services.domain;
+package com.iwd.petstore.services.domain;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
