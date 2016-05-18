dc73fbb7cbe2b8767d7c0020086624c5d1e6bc6f
Ernie Shu <ernie.shu@intelliware.com>
2016-05-18 12:29:58 -0400

    Improve look and feel of app.

--- petstore-ui/app/scripts/controllers/main.js
+++ petstore-ui/app/scripts/controllers/main.js
@@ -10,18 +10,27 @@
 angular.module('petstoreUiApp')
   .controller('MainCtrl', function ($scope, $http) {
 
-  	console.log("Starting a console log of this working...!");
-
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
+    $scope.types = [
+      'Type 0', 
+      'Type 1',
+      'Type 2', 
+      'Type 3'
     ];
 
     $scope.addAPet = function() {
     	console.log("adding a pet with name:" + $scope.name);
 
-    	// $http.post('pet');
+    	var petToBeAdded = {
+    		name : $scope.name,
+        category : {
+          id: $scope.categoryId
+        },
+        status : $scope.status
+    	};
+    	$http.post('pet', petToBeAdded).success(function(data) {
+	  		// if we've gotten the pet, map it back to the GUI
+	  		$scope.petName = data.name;
+	  	});
     };
 
     this.findAPet = function(data) {
