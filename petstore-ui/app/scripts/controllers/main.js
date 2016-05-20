1ea34a79eea375e066bf04c2ace1d8e1bd6cd940
Ernie Shu <ernie.shu@intelliware.ca>
2016-05-20 13:49:14 -0400

    Displaying everything successfully.

--- petstore-ui/app/scripts/controllers/main.js
+++ petstore-ui/app/scripts/controllers/main.js
@@ -58,10 +58,18 @@ angular.module('petstoreUiApp')
 	  	});
     };
 
-    this.findAPet = function(data) {
-	  	$http.get('pet/' + data.petId).success(function(data) {
+    $scope.findAPet = function(data) {
+	  	$http.get('pet/' + $scope.searchPetId).success(function(data) {
+        console.log("Retrieving info for pet name: " + data.name);
 	  		// if we've gotten the pet, map it back to the GUI
-	  		$scope.petName = data.name;
+        $scope.viewPet = {
+          id: data.id,
+          name: data.name,
+          category: data.category,
+          status: data.status,
+          tags: data.tags,
+          photoUrls: data.photoUrls
+        };
 	  	});
     };
   });
