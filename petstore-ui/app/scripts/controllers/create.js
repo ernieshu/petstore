9e1e25bc64d9d4ec2dec601e6c0c07f40b976c9b
Ernie Shu <ernie.shu@intelliware.ca>
2016-05-31 16:11:21 -0400

    Repair controller for create.

--- petstore-ui/app/scripts/controllers/create.js
+++ petstore-ui/app/scripts/controllers/create.js
@@ -2,22 +2,22 @@
 angular.module('petstoreUiApp')
   .controller('CreateCtrl', function ($scope, $http) {
 
-    // this.categories = [
-    //   { id: 0, name: 'Category 0'},
-    //   { id: 1, name: 'Category 1'},
-    //   { id: 2, name: 'Category 2'},
-    //   { id: 3, name: 'Category 3'}
-    // ];
+    this.categories = [
+      { id: 0, name: 'Category 0'},
+      { id: 1, name: 'Category 1'},
+      { id: 2, name: 'Category 2'},
+      { id: 3, name: 'Category 3'}
+    ];
 
-    // this.tags = [
-    //   { id: 0, name: 'Tag 0'},
-    //   { id: 1, name: 'Tag 1'},
-    //   { id: 2, name: 'Tag 2'},
-    //   { id: 3, name: 'Tag 3'}
-    // ];
+    this.tags = [
+      { id: 0, name: 'Tag 0'},
+      { id: 1, name: 'Tag 1'},
+      { id: 2, name: 'Tag 2'},
+      { id: 3, name: 'Tag 3'}
+    ];
 
-    // this.pet = {
-    // };
+    this.pet = {
+    };
 
     this.addAPet = function() {
 
