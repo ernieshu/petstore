e879e0bfdd4f2e18b6e3b799daf03b021ce6c6f7
Ernie Shu <ernie.shu@intelliware.ca>
2016-06-06 16:29:52 -0400

    Trying to address serialization issue, which is only seen on Linux.

--- petstore-services/src/main/java/com/iwd/petstore/services/dao/domain/PetStatus.java
+++ petstore-services/src/main/java/com/iwd/petstore/services/dao/domain/PetStatus.java
@@ -1,7 +1,31 @@
 package com.iwd.petstore.services.dao.domain;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import com.fasterxml.jackson.annotation.JsonValue;
+
 public enum PetStatus {
 
 	AVAILABLE, PENDING, SOLD;
 
+	private static Map<String, PetStatus> namesMap = new HashMap<String, PetStatus>(3);
+	
+    static {
+        namesMap.put("AVAILABLE", AVAILABLE);
+        namesMap.put("PENDING", PENDING);
+        namesMap.put("SOLD", SOLD);
+    }	
+	
+    @JsonValue
+    public String toValue() {
+        for (Entry<String, PetStatus> entry : namesMap.entrySet()) {
+            if (entry.getValue() == this)
+                return entry.getKey();
+        }
+
+        return null; // or fail
+    }	
+	
 }
